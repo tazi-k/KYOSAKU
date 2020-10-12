72a3e91a7f095f7a838335ecd25286785739d51b
tazi-k <tajitoc14@gmail.com>
2020-10-12 13:08:20 +0900

    change

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,8 +29,8 @@ class AppServiceProvider extends ServiceProvider
         //ローカルの時はif消す
         if(env('APP_ENV') === 'production')
         {
-            $genres_music = Genre::where('id', [1,11,21,31,41,51,61,71,91,12,22,32,42,52])->get();
-            $genres_illustration = Genre::where('id',[62])->get();
+            $genres_music = Genre::where('id','<',151)->get();
+            $genres_illustration = Genre::where('id','>',141)->get();
             View::share(compact('genres_music','genres_illustration'));
         }
         Schema::defaultStringLength(191);
