0bc7d1ce582f3da54f79ddad986bdd5748bcd0de
tazi-k <tajitoc14@gmail.com>
2020-10-12 12:48:00 +0900

    change

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,10 +29,17 @@ class AppServiceProvider extends ServiceProvider
         //ローカルの時はif消す
         if(env('APP_ENV') === 'production')
         {
-            $genres_music = Genre::where('id','<',15)->get();
-            $genres_illustration = Genre::where('id','>',14)->get();
+            $genres_music = Genre::where('id',[1,11,21,31,41,51,61,71,91,12,22,32,42,52])->get();
+            $genres_illustration = Genre::where('id',[62,72,82,92,13,23,33,43,53,63,73,83,93,])->get();
             View::share(compact('genres_music','genres_illustration'));
         }
         Schema::defaultStringLength(191);
     }
 }
+// $genres_music = Genre::where('id',[1,11,21,31,41,51,61,71,91,12,22,32,42,52])->get();
+//         $genres_illustration = Genre::where('id',[62,72,82,92,13,23,33,43,53,63,73,83,93,])->get();
+
+
+        // $genres_music = Genre::where('id','<',15)->get();
+        //     $genres_illustration = Genre::where('id','>',14)->get();
+        //     View::share(compact('genres_music','genres_illustration'));
