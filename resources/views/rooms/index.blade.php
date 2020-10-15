6f8e1f07d44861f68b1ecfc70765b2e1d0ceaa55
tazi-k <tajitoc14@gmail.com>
2020-10-15 19:59:42 +0900

    change

--- resources/views/rooms/index.blade.php
+++ resources/views/rooms/index.blade.php
@@ -25,7 +25,6 @@
             <form action="{{ route('chats.store',$room) }}" method="POST">
                 {{csrf_field()}}
                 <div class="form-group">
-                    <label>内容</label>
                     <textarea class="form-control" placeholder="内容" rows="5" name="body"></textarea>
                 </div>
                 <input type="hidden" name="matching" value="{{ $matching->id }}">
