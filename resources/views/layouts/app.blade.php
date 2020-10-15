09488156bee5365a874254a1bde92097e0304a4f
tazi-k <tajitoc14@gmail.com>
2020-10-16 08:20:17 +0900

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     
-    <link rel="icon" href="{{ asset('/favicon.ico') }} />
+    <link rel="icon" href="{{ asset('/favicon.ico') }}" />
 
     <!-- Styles -->
 
