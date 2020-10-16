9f3ace42dff6af4ba6086f4ebe69526c6e95f4a5
tazi-k <tajitoc14@gmail.com>
2020-10-16 23:26:33 +0900

    header-change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,7 +69,6 @@
                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
-                        
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                             <a style="color: black; margin-left: 0; text-align:center" class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
