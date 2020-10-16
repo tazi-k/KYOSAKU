74e3e400f2eea213a083217b3730ca8046c725f3
tazi-k <tajitoc14@gmail.com>
2020-10-16 10:33:19 +0900

    puroduction-change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,23 +37,7 @@
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 
 {{-- favicon --}}
-    <link rel="apple-touch-icon" sizes="57x57" href="../images/favicon/apple-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="../images/favicon/apple-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="72x72" href="../images/favicon/apple-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="76x76" href="../images/favicon/apple-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="114x114" href="../images/favicon/apple-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="120x120" href="../images/favicon/apple-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="144x144" href="../images/favicon/apple-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="152x152" href="../images/favicon/apple-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="180x180" href="../images/favicon/apple-icon-57x57.png">
-<link rel="icon" type="image/png" sizes="192x192"  href="../images/favicon/apple-icon-57x57.png">
-<link rel="icon" type="image/png" sizes="32x32" href="../images/favicon/apple-icon-57x57.png">
-<link rel="icon" type="image/png" sizes="96x96" href="../images/favicon/apple-icon-57x57.png">
-<link rel="icon" type="image/png" sizes="16x16" href="../images/favicon/apple-icon-57x57.png">
-<link rel="manifest" href="../images/favicon/apple-icon-57x57.png">
-<meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
-<meta name="theme-color" content="#ffffff">
+<link rel="icon" type="image/png" sizes="48x48" href="../images/favicon.ico">
     
     
 </head>
