e3535bcb505685ad7a3bbe5e0bc3bc1d036f0726
tazi-k <tajitoc14@gmail.com>
2020-11-24 16:32:51 +0900

    responsive-LP

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,12 +27,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         // ローカルの時はif消す
-        if(env('APP_ENV') === 'production')
-        {
-            $genres_music = Genre::where('id','<',141)->get();
-            $genres_illustration = Genre::where('id','>',131)->get();
-            View::share(compact('genres_music','genres_illustration'));
-        }
+        // if(env('APP_ENV') === 'production')
+        // {
+        //     $genres_music = Genre::where('id','<',141)->get();
+        //     $genres_illustration = Genre::where('id','>',131)->get();
+        //     View::share(compact('genres_music','genres_illustration'));
+        // }
 
         $genres_music = Genre::where('id','<',15)->get();
             $genres_illustration = Genre::where('id','>',14)->get();
